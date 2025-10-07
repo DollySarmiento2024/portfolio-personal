4f06bd95226179e3dbb2be81197deac238a9ffbc
fernandasar1289 <fernandasar1289@gmail.com>
2025-10-07 22:37:34 +0000

    video en seccion hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -2,18 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  template: `
-    <section id="hero">
-      <video autoplay muted loop id="myVideo">
-        <source src="/assets/video-nebulosa.mp4" type="video/mp4">
-      </video>
-      <div class="content">
-        <h1 class="display-3 fw-bold">Fernanda Sarmiento</h1>
-        <p class="lead fs-2">Desarrolladora Web Fullstack</p>
-        <a href="#contact" class="btn btn-primary btn-lg mt-4">Contáctame</a>
-      </div>
-    </section>
-  `,
+  templateUrl: './hero.html',
   styleUrls: ['./hero.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
